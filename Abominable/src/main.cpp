c1c6a557dfef09c1452ca18a275d40b067f2cf50
thecodepro90 <97404537+thecodepro90@users.noreply.github.com>
2024-11-07 20:01:23 -0700

    Add intake task

--- Abominable/src/main.cpp
+++ Abominable/src/main.cpp
@@ -1,9 +1,14 @@
 #include "main.h"
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::MotorGroup left_mg({10,19});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-pros::MotorGroup right_mg({9, 20});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+pros::MotorGroup left_mg({10,19}); 
+pros::MotorGroup right_mg({9, 20});
 pros::ADIDigitalOut piston(1);
+pros::Motor intake(16, pros::v5::MotorGears::blue);
+pros::Motor hook(6, pros::v5::MotorGears::green);
+
+
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -72,19 +77,35 @@ void toggle_piston_fn() {
 		
 }
 
+void toggle_intake_fn() {
+	while (true) {
+		if (master.get_digital(DIGITAL_L2)) 
+		{
+			intake.move_velocity(pros::E_MOTOR_GEAR_BLUE);
+			hook.move_velocity(pros::E_MOTOR_GEAR_BLUE);
+		}
+		else if (master.get_digital(DIGITAL_L1)) 
+		{
+			intake.move_velocity(-pros::E_MOTOR_GEAR_BLUE);
+			hook.move_velocity(-pros::E_MOTOR_GEAR_BLUE);
+		}
+		else
+		{
+			intake.brake();
+			hook.brake();
+		}
+		
+	}
+}
 
 void opcontrol() {
 	
 
 	pros::Task piston_toggle(toggle_piston_fn);
-
+	pros::Task intake_toggle(toggle_intake_fn);
 	
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
